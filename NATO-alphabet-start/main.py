9f6fa30bd7a09f88fad9d887eb94670caf1f4a02
ZakiID18 <zakiid18@gmail.com>
2021-08-03 12:47:51 +0700

    NATO Alphabet

--- NATO-alphabet-start/main.py
+++ NATO-alphabet-start/main.py
@@ -5,10 +5,10 @@ data = pandas.read_csv("nato_phonetic_alphabet.csv")
 # Keyword Method with iterrows()
 # {new_key:new_value for (index, row) in df.iterrows()}
 
-#TODO 1. Create a dictionary in this format:
+# TODO 1. Create a dictionary in this format:
 phonetic_dict = {row.letter:row.code for (index, row) in data.iterrows()}
 
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+# TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 # Can use function or make a while loop
 def generate_phonetic():
     word = input("Enter a word: ").upper()
